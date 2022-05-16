0a7703a3985c98a20073f3223a8c14b854e33263
Subodh20 <subodhtiwari360@gmail.com>
2022-05-17 09:13:27 +1000

    simple signup design

--- ground-zero/src/components/login/LoginPage.jsx
+++ ground-zero/src/components/login/LoginPage.jsx
@@ -1,53 +1,67 @@
-import {Link} from "react-router-dom";
-import {useState} from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function Login({isLoggedIn}) {
+function Login({ isLoggedIn }) {
+  const userAdmin = {
+    username: "admin",
+    password: "admin",
+  };
 
-    const userAdmin = {
-        username:"admin",
-        password:"admin"
+  const [loginDetails, setLoginDetails] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (
+      userAdmin.username === loginDetails.username &&
+      userAdmin.password === loginDetails.password
+    ) {
+      console.log("authenticated");
+      isLoggedIn(true);
+    } else {
     }
+  };
 
-    const [loginDetails, setLoginDetails] = useState({username: "", password: ""});
+  const handleChange = ({ currentTarget: input }) => {
+    const account = { ...loginDetails };
+    account[input.name] = input.value;
+    setLoginDetails(account);
+  };
 
-    const handleLogin = e => {
-        e.preventDefault();
-        if(userAdmin.username === loginDetails.username && userAdmin.password === loginDetails.password){
-            console.log("authenticated");
-            isLoggedIn(true);
-        }else{
-            console.log("username or password is incorrect");
-        }
-    }
-
-    const handleChange = ({currentTarget:input}) => {
-        const account = {...loginDetails};
-        account[input.name]=input.value;
-        setLoginDetails(account);
-    }
-
-
-    return <div>
-        <p>Welcome to Login Page</p>
-        <form onSubmit={handleLogin}>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" className="form-control"
-                       value={loginDetails.username}
-                       name="username"
-                       onChange={handleChange}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input id="password" type="text" className="form-control"
-                       value={loginDetails.password}
-                       name="password"
-                       onChange={handleChange}/>
-            </div>
-            <button className="btn btn-primary">Login</button>
-        </form>
-        <span>Don't have an account? </span><Link to="/sign-up">create new account?</Link>
+  return (
+    <div>
+      <p>Welcome to Login Page</p>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            className="form-control"
+            value={loginDetails.username}
+            name="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="text"
+            className="form-control"
+            value={loginDetails.password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn btn-primary">Login</button>
+      </form>
+      <span>Don't have an account? </span>
+      <Link to="/sign-up">create new account?</Link>
     </div>
+  );
 }
 
 export default Login;
--- ground-zero/src/components/login/SignUpPage.jsx
+++ ground-zero/src/components/login/SignUpPage.jsx
@@ -1,10 +1,28 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
-    return <div>
-        <p>this is signup page</p>
-        <Link to ="/login" >Login</Link>
-    </div>
+  return (
+    <form className="m-2">
+      <div className="mb-3">
+        <label htmlFor="user-name">UserName</label>
+        <input className="form-control" id="user-name" type="text" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="user-email">Email</label>
+        <input className="form-control" id="user-email" type="email" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="user-password">Password</label>
+        <input className="form-control" id="user-password" type="password" />
+      </div>
+      <div className="mb-3">
+        <button type="submit" className="btn btn-primary m-2">
+          Signup
+        </button>
+        <Link to="/login">Already have account?</Link>
+      </div>
+    </form>
+  );
 }
 
 export default SignUpPage;
